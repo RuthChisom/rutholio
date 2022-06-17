73e57e5b1fdcb639be757a36440a6b2e1512de44
Ruth Chisom <ndukwe100805043@gmail.com>
2022-06-17 22:25:49 +0100

    A portfolio having HTML pages
    Create a portfolio having the following HTML pages:
    
    Home: The homepage should contain your data (bio)
    About: The about me page should contain brief information about you
    Contact: The contact page should contain links to your social media
    
    Create a server using the HTTP module
    
    Create routes that render the HTML pages you created on a browser
    
    The base URL of your app should render the homepage
    
    there should be a /home route that redirects to the base of your app
    
    Push your code to GitHub.
    
    Note:
    
    1. No third-party dependencies
    
    2. You don’t need to style your HTML pages

---
+++ index.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const fs = require('fs');
+
+//create a server with HTTP variable
+const server = http.createServer(function(req,res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    // home page
+    if(req.url=='/' || req.url=='/index.html' || req.url=='/home'){
+        fs.readFile('./index.html', null, function(err, data){
+            if(err){
+                return res.status(500).json({message: err}) ;//internal server error
+            }
+            res.write(data);
+            res.end();
+        })
+    }
+    //about page
+    if(req.url=='/about.html' ){
+        fs.readFile('./about.html', null, function(err, data){
+            if(err){
+                return res.status(500).json({message: err}) ;
+            }
+            res.write(data);
+            res.end();
+        })
+    }
+    //contact page
+    if(req.url=='/contact.html' ){
+        fs.readFile('./contact.html', null, function(err, data){
+            if(err){
+                return res.status(500).json({message: err}) ;
+            }
+            res.write(data);
+            res.end();
+        })
+    }
+
+    });
+
+//create a port
+server.listen(4005, 'localhost'); 
+
+console.log("You have successfully created a server!");
